ff439ba746955549c0b30484b4c7c5a186fe6985
jeremiahlukus <jeremiahlukus1@gmail.com>
2018-01-31 12:25:34 -0500

    First

---
+++ app/models/film.rb
@@ -0,0 +1,13 @@
+class Film < CDQManagedObject
+  scope :released, sort_by(:release_date)
+
+  def cell 
+    {
+      title: title, 
+      subtitle: "Directed by: #{director}",
+      action: :show,
+      arguments: { episode_id: episode_id},
+      accessory_type: :disclosure_button
+    }
+  end 
+end
